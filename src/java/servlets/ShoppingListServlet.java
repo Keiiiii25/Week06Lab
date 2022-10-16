cc32bc745409a291618572c22b83477c8c1a5073
Keiiiii25 <102275393+Keiiiii25@users.noreply.github.com>
2022-10-16 09:28:53 -0600

    Displaying jsp properly

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -15,6 +15,7 @@ public class ShoppingListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
 
     @Override
